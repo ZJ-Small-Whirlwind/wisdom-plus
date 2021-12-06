ef340fc7c577f0c29b56f2454586c166f3386c39
zys8119 <770959294@qq.com>
2021-12-06 15:11:54 +0800

    [add] button

--- packages/components/Button/index.ts
+++ packages/components/Button/index.ts
@@ -1,4 +1,4 @@
-import { withInstall, withNoopInstall } from '@wisdom-plus/utils/with-install'
+import { withInstall } from '@wisdom-plus/utils/with-install'
 import Button from './src/button.vue'
 
 export const WpButton = withInstall(Button, )
