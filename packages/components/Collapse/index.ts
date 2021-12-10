b3281f542c554f564e838780e54f51e47ca35ede
chentao <jooies@qq.com>
2021-12-10 11:03:33 +0800

    [mod] export type

--- packages/components/Collapse/index.ts
+++ packages/components/Collapse/index.ts
@@ -3,3 +3,6 @@ import collapse from './src/collapse'
 
 export const WpCollapse = withInstall(collapse)
 export default WpCollapse
+
+export { collapseEmits, collapseProps } from './src/collapse'
+export type { CollapseSupport, CollapseProps, CollapseEmits } from './src/collapse'
--- packages/components/CollapseItem/index.ts
+++ packages/components/CollapseItem/index.ts
@@ -3,3 +3,6 @@ import collapseItem from './src/collapseItem'
 
 export const WpCollapseItem = withInstall(collapseItem)
 export default WpCollapseItem
+
+export { collapseItemProps } from './src/collapseItem'
+export type { CollapseItemProps } from './src/collapseItem'
