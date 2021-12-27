45fd15e5433159e2062743bb8885643ada9ba488
chentao <jooies@qq.com>
2021-12-27 08:48:16 +0800

    [fix] fix bug

--- packages/components/Modal/src/modal.tsx
+++ packages/components/Modal/src/modal.tsx
@@ -22,6 +22,9 @@ export const modalProps = buildProps({
     width: {
         type: [String, Number] as PropType<string | number>
     },
+    height: {
+        type: [String, Number] as PropType<string | number>
+    },
     showClose: {
         type: Boolean,
         default: true
@@ -48,7 +51,8 @@ export const modalProps = buildProps({
     from: {
         type: String as PropType<'top' | 'bottom' | 'left' | 'right'>,
         default: 'bottom'
-    }
+    },
+    handler: Boolean
 })
 
 export type ModalProps = ExtractPropTypes<typeof modalProps>
@@ -65,6 +69,7 @@ export default defineComponent({
         afterOpen: null
     },
     setup(props, { emit, attrs, slots }) {
+        const modalRef = ref<HTMLDivElement | null>(null)
         const showRef = ref(false)
         const show = computed<boolean>({
             get: () => {
@@ -112,24 +117,105 @@ export default defineComponent({
                 show.value = false
             }
         })
+        /**
+         * 手势关闭
+         */
+         const startX = ref(0)
+         const startY = ref(0)
+         const deltaX = ref(0)
+         const deltaY = ref(0)
+         const transitionDuration = ref('')
+         const needTouchEvent = computed(() => props.handler)
+         const handleTouchStart = (e: TouchEvent) => {
+             if (!needTouchEvent.value) return
+             transitionDuration.value = 'none'
+             startX.value = e.touches[0].pageX
+             startY.value = e.touches[0].pageY
+         }
+         const handleTouchMove = (e: TouchEvent) => {
+             if (!needTouchEvent.value) return
+             deltaX.value = e.touches[0].pageX - startX.value
+             deltaY.value = e.touches[0].pageY - startY.value
+             /**
+              * 往上拖的逻辑暂时取消
+              */
+             // if (props.from === 'left') {
+             //     if (deltaX.value < -40) deltaX.value = -40
+             // } else if (deltaX.value > 40) deltaX.value = 40
+             // if (props.from === 'bottom') {
+             //     if (deltaY.value < -40) deltaY.value = -40
+             // } else if (deltaY.value > 40) deltaY.value = 40
+         }
+         const clear = (close = false) => {
+             deltaX.value = 0
+             deltaY.value = 0
+             if (close) {
+                 nextTick(() => {
+                     show.value = false
+                 })
+             }
+         }
+         const handleTouchEnd = () => {
+             if (!needTouchEvent.value) return
+             transitionDuration.value = '.3s'
+             if (modalRef.value) {
+                 if (props.from === 'bottom' || props.from === 'top') {
+                     if (Math.abs(deltaY.value) > modalRef.value?.clientHeight / 2) {
+                         return clear(true)
+                     }
+                 }
+                 if (Math.abs(deltaX.value) > modalRef.value?.clientWidth / 2) {
+                     return clear(true)
+                 }
+             }
+             clear()
+         }
+         const modalTransform = computed(() => {
+             switch (props.from) {
+                 case 'bottom':
+                     return deltaY.value > 0 ? `translateY(${deltaY.value}px)` : ''
+                 case 'top':
+                     return deltaY.value < 0 ? `translateY(${deltaY.value}px)` : ''
+                 case 'left':
+                     return deltaX.value < 0 ? `translateX(${deltaX.value}px)` : ''
+                 case 'right':
+                     return deltaX.value > 0 ? `translateX(${deltaX.value}px)` : ''
+             }
+         })
         return () => (
             <Overlay {...props.overlay} modelValue={showOverlay.value} onUpdate:modelValue={(value) => { show.value = value }} class={{
                 'wp-modal__overlay': true,
                 [`wp-modal__overlay-${props.from}`]: props.type === 'drawer'
             }}>
-                <Transition name={props.transitionName || (props.type === 'drawer' ? `wp-modal-${props.from}` : 'wp-modal-fade' )} onAfterLeave={() => emit('afterClose')} onAfterEnter={() => emit('afterOpen')}>
+                <Transition name={props.transitionName || (props.type === 'drawer' ? `wp-modal-${props.from}` : 'wp-modal-fade' )} onAfterLeave={() => emit('afterClose')} onAfterEnter={() => {
+                    modalRef.value?.focus()
+                    emit('afterOpen')
+                }}>
                     {
                         showBox.value ? (
-                            <div class={{
-                                'wp-modal': true,
-                                'wp-modal__no-border': !props.border,
-                                'wp-modal__drawer': props.type === 'drawer',
-                                [`wp-modal__drawer-${props.from}`]: props.type === 'drawer'
-                            }} style={{
-                                display: !showBox.value ? 'none' : '',
-                                width: typeof props.width === 'string' ? props.width : `${props.width}px`,
-                                '--wp-modal-border-radius': props.borderRadius === true ? '4px' : props.borderRadius === false ? 0 : props.borderRadius
-                            } as CSSProperties} onClick={e => e.stopPropagation()} {...attrs}>
+                            <div
+                                class={{
+                                    'wp-modal': true,
+                                    'wp-modal__no-border': !props.border,
+                                    'wp-modal__drawer': props.type === 'drawer',
+                                    [`wp-modal__drawer-${props.from}`]: props.type === 'drawer'
+                                }} style={{
+                                    display: !showBox.value ? 'none' : '',
+                                    width: typeof props.width === 'string' ? props.width : `${props.width}px`,
+                                    height: typeof props.height === 'string' ? props.height : `${props.height}px`,
+                                    transform: modalTransform.value,
+                                    transition: transitionDuration.value,
+                                    '--wp-modal-border-radius': props.borderRadius === true ? '4px' : props.borderRadius === false ? 0 : props.borderRadius
+                                } as CSSProperties}
+                                onClick={e => e.stopPropagation()}
+                                ref={modalRef}
+                                v-show={props.overlay.useVShow ? showBox.value : true}
+                                tabindex="-1"
+                                onTouchstart={handleTouchStart}
+                                onTouchmove={handleTouchMove}
+                                onTouchend={handleTouchEnd}
+                                {...attrs}
+                            >
                                 {
                                     slots.title || props.title || props.showClose ? (
                                         <div class="wp-modal__header">
