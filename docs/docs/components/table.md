e5ed3ab26ebd73f6f75de41703f083820945f3ff
zys8119 <770959294@qq.com>
2022-02-18 11:30:57 +0800

    [add] table

--- docs/docs/components/table.md
+++ docs/docs/components/table.md
@@ -29,45 +29,36 @@ const columns = ref([
     {label:"a", prop:"name",columns:[
         {label:"a-1", prop:"name"},
         {label:"a-2", prop:"age"},
-        {label:"a-3", btns:[
+        {label:"a-3", prop:"age",btns:[
             {name:'编辑'},
         ]},
     ]},
     {label:"b", prop:"age"},
-    {label:"c", btns:[
+    {label:"c", prop:"age", btns:[
         {name:'编辑'},    
     ]},
-    {label:"d", btns:[
+    {label:"d", prop:"age", btns:[
         {name:'编辑'},    
     ],columns:[
         {label:"d-1", prop:"name"},
         {label:"d-2", prop:"age"},
-        {label:"d-3", btns:[
+        {label:"d-3", prop:"age" , btns:[
             {name:'编辑'},
         ],columns:[
-            {label:"d-3-1", prop:"name"},
+            {label:"d-3-1", prop:"name",columns:[
+                {label:"asda", prop:"age"},
+                {label:"asda", prop:"age"},
+            ]},
             {label:"d-3-2", prop:"age"},
-            {label:"d-3-3", btns:[
-                    {name:'编辑'},
-                ]},
+            {label:"d-3-3", prop:"age", btns:[
+                {name:'编辑'},
+            ]},
         ]},
     ]},
 ])
-columns.value = [
-    {lable:"日期"},
-    {label:"配送信息",columns:[
-        {label:"姓名",},
-        {label:"地址",columns:[
-            {label:"省份",},
-            {label:"市区",},
-            {label:"地址",},
-            {label:"邮编",},
-        ]},
-    ]},
-]
 const data = ref([
-    {name:'张三', arg:21},
-    {name:'小明', arg:18},
+    {name:'张三', age:21},
+    {name:'小明', age:18},
 ])
 </script>
 ```
--- packages/components/Table/src/table.tsx
+++ packages/components/Table/src/table.tsx
@@ -18,64 +18,111 @@ export default defineComponent({
     name: 'WpTable',
     props: tableProps,
     setup(props) {
+        /**
+         * 获取合并单元格栏目数据
+         * @param columns
+         */
+        const getColumnsMergedCell = (columns)=>{
+            let rowspanMax = 0;
+            let colspanMax = 0;
+            let columns_col = [];
+            let columnsMap = {};
+            /**
+             * 平铺单元格栏目
+             * @param itemColumns 单元格栏目集合
+             * @param result 返回平铺数据
+             */
+            const flatDry:any = (itemColumns,result:any = []) =>{
+                itemColumns.forEach(it=>{
+                    if(it.columns.length > 1){
+                        flatDry(it.columns,result)
+                    }else {
+                        result.push(it);
+                    }
+                })
+                return result;
+            }
+            /**
+             * 获取单元格栏目信息
+             * @param columns 栏目
+             * @param level 栏目层深
+             */
+            const getItemColumns = (columns, level = 0)=>{
+                return columns.map(it=>{
+                    const levelIndex = level+1;
+                    rowspanMax = levelIndex > rowspanMax ? levelIndex : rowspanMax;
+                    colspanMax = it.columns ? colspanMax : colspanMax + 1;
+                    columnsMap[levelIndex] = columnsMap[levelIndex] || [];
+                    const itemColumns = getItemColumns(it.columns || [],levelIndex);
+                    let colspanArr = flatDry(itemColumns);
+                    const item = {
+                        ...it,
+                        level:levelIndex,
+                        columns:itemColumns,
+                        colspanArr,
+                        colspan:colspanArr.length || 1,
+                    }
+                    if(!it.columns){
+                        columns_col.push(item);
+                    }
+                    columnsMap[levelIndex].push(item);
+                    return item;
+                });
+            }
+            getItemColumns(columns);
+            return {
+                // 栏目映射
+                columnsMap,
+                // 总行数
+                rowspanMax,
+                // 总列数
+                colspanMax,
+                // 栏目列集合
+                columns_col,
+            }
+        }
         return {
-
+            theadColumns:getColumnsMergedCell(props.columns)
         }
     },
     render() {
-        let rowspan = 0;
-        let colspan = 0;
-        let rowsIndexMap = {};
-        const getThead = (columns, level = 0)=>{
-            return columns.map(it=>{
-                const levelIndex = level+1;
-                rowspan = levelIndex > rowspan ? levelIndex : rowspan;
-                colspan = it.columns ? colspan : colspan + 1;
-                rowsIndexMap[levelIndex] = rowsIndexMap[levelIndex] || [];
-                const itemColumns = getThead(it.columns || [],levelIndex);
-                const item = {
-                    ...it,
-                    level:levelIndex,
-                    columns:itemColumns,
-                }
-                rowsIndexMap[levelIndex].push(item);
-                return item
-            });
-        }
-        console.log(getThead(this.columns), rowspan, colspan,rowsIndexMap)
         const theadRender = ()=>(<thead>
-            {Object.values(rowsIndexMap).map((item:any,key:number)=>(
+            {Object.values(this.theadColumns.columnsMap).map((item:any,key:number)=>(
                 <tr>
-                    {item.map((it)=>(
-                        <th rowspan={rowspan - key - it.columns.length}>
-                            <div>{it.label}</div>
+                    {item.map((column)=>(
+                        <th class={{
+                                "wp-table__cell":true,
+                            }}
+                            colspan={column.colspan}
+                            rowspan={column.colspan === 1 ? this.theadColumns.rowspanMax-key:1}>
+                            <div class={{
+                                "cell":true
+                            }}>{column.label}</div>
                         </th>
                     ))}
                 </tr>
             ))}
-            {/*<tr>*/}
-            {/*    <th colspan={1} rowspan={3}>1</th>*/}
-            {/*    <th colspan={5} rowspan={1}>2</th>*/}
-            {/*</tr>*/}
-            {/*<tr>*/}
-            {/*    <th colspan={1} rowspan={2}>4</th>*/}
-            {/*    <th colspan={4} rowspan={1}>5</th>*/}
-            {/*</tr>*/}
-            {/*<tr>*/}
-            {/*    <th colspan={1} rowspan={1}>7</th>*/}
-            {/*    <th colspan={1} rowspan={1}>8</th>*/}
-            {/*    <th colspan={1} rowspan={1}>9</th>*/}
-            {/*    <th colspan={1} rowspan={1}>10</th>*/}
-            {/*</tr>*/}
         </thead>)
         const tbodyRender = ()=>(<tbody>
-
+            {this.data.map(row=>(
+                <tr>
+                    {this.theadColumns.columns_col.map((column:any)=>(
+                        <td class={{
+                                "wp-table__cell":true,
+                            }}>
+                            <div class={{
+                                "cell":true
+                            }}>{row[column.prop]}{column.prop}</div>
+                        </td>
+                    ))}
+                </tr>
+            ))}
         </tbody>)
         return (
             <div class={'wp-table'}>
                 <table class={{
                     'wp-table--body': true,
-                }} border>
+                }} border={0} cellpadding={0} cellspacing={0}>
                     {theadRender()}
                     {tbodyRender()}
                 </table>
