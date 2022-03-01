f5a86fffe66447a50be381ad06cd0bf6058ee43c
chentao <jooies@qq.com>
2022-03-01 09:20:34 +0800

    [mod] no warning

--- packages/components/CollapseTransition/index.ts
+++ packages/components/CollapseTransition/index.ts
@@ -1,9 +1,8 @@
 import { withInstall } from '@wisdom-plus/utils/with-install'
 import collapseTransition from './src/collapseTransition'
 
-export const WpCollapseTransition = withInstall(collapseTransition)
-export const CollapseTransition = withInstall(collapseTransition)
-export default WpCollapseTransition
+export const CollapseTransition = withInstall(collapseTransition, ['WpCollapseTransition'])
+export default CollapseTransition
 
 export { collapseTransitionProps } from './src/collapseTransition'
 export type { CollapseTransitionProps } from './src/collapseTransition'
