a9c8b3be6a2418c1d1f71c371444df27faacff04
zys8119 <770959294@qq.com>
2022-03-02 15:08:10 +0800

    [add] select 选择器

--- packages/components/Select/src/select.tsx
+++ packages/components/Select/src/select.tsx
@@ -1,41 +1,58 @@
-import {defineComponent, ExtractPropTypes, ref, computed} from "vue"
+import {defineComponent, ExtractPropTypes, ref, computed, h} from "vue"
 import {buildProps, definePropType} from "@wisdom-plus/utils/props";
 import {DownOutlined} from "@vicons/antd"
 import WpInput from "../../Input"
 import WpIcon from "../../Icon"
+import WpButton from "../../Button"
 import WpPopover from "../../Popover"
 export const selectProps = buildProps({
     options:{
         type:definePropType<Array<object>>([Array]),
         default:()=>[]
+    },
+    labelName:{
+        type:definePropType<string>(String),
+        default:'label'
+    },
+    modelValue:{
+        type:definePropType<Array<object> | string | number | boolean>([Array, String, Number, Boolean]),
+        default:null
     }
 })
 export type SelectProps = ExtractPropTypes<typeof selectProps>
 export default defineComponent({
     name:"WpSelect",
     props:selectProps,
+    inheritAttrs:false,
     setup(){
         const readonly = ref(true);
-        const show = ref(false);
+        const show = ref(true);
         const input = ref(null);
-        const dark = ref(false);
+        const valueStr = computed(()=>"asdas");
+        const optionClick = ({item,ev})=>{
+            show.value = false;
+            console.log(item)
+        }
         return {
             input,
             readonly,
             show,
-            dark,
+            optionClick,
+            valueStr,
         }
     },
     render(){
         return (<WpPopover
+                   v-model={this.show}
                    placement={'bottom-start'}
                    arrow={false}
+                   popoverClass={'wp-select-panel-popover'}
                    v-slots={{
                         reference:()=>(<div class={{
                             'wp-select': true,
                             'wp-select-show': this.show,
                         }}>
-                            <WpInput readonly={this.readonly} ref={'input'} placeholder={'请选择'} class={{
+                            <WpInput v-model={this.valueStr} readonly={this.readonly} ref={'input'} placeholder={'请选择'} class={{
                                 'wp-select-input': true,
                                 'wp-select-show-input': this.show,
                             }} v-slots={{
@@ -46,7 +63,20 @@ export default defineComponent({
                             }}></WpInput>
                         </div>)
                    }}
-        >asdasd----{String(this.show)}
+        >
+            <div class={{
+                'wp-select-panel': true,
+            }}>
+                {this.$props.options.map(item=>(
+                <div
+                    onClick={ev=>this.optionClick({item,ev})}
+                    class={{
+                    'wp-select-panel-option': true,
+                }}>
+                    {this.$props.labelName}
+                    {item[this.$props.labelName]}
+                </div>))}
+            </div>
         </WpPopover>)
     }
 })
