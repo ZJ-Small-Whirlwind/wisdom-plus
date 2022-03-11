0d5dd43331ea9028c7f2cec6c8f626244d7ebd07
zys8119 <770959294@qq.com>
2022-03-11 12:47:06 +0800

    [add] date-picker 日期选择器

--- packages/components/DatePicker/src/DatePicker.tsx
+++ packages/components/DatePicker/src/DatePicker.tsx
@@ -17,14 +17,13 @@ export default defineComponent({
         const refSelect:any = ref(null)
         const onClickDay = ({year, month, date})=>{
             refSelect.value.show = false;
-            const value = dayjs(new Date(year.value,month.value,date.value)).format("YYYY-MM-DD")
+            const value = dayjs(new Date(year.value,month.value-1,date.value)).format("YYYY-MM-DD")
             options.value = [
                 {label:value,value},
             ];
             nextTick(()=>{
                 currentValue.value = value;
             })
-            console.log(options.value, currentValue.value)
         }
         return {
             onClickDay,
