99cbbaf7713ebfa9f5e0ddf2e78721ffb4c2e783
chentao <jooies@qq.com>
2022-03-21 16:08:32 +0800

    [fix] path error

--- packages/components/Popover/src/popover.tsx
+++ packages/components/Popover/src/popover.tsx
@@ -110,7 +110,7 @@ export default defineComponent({
         const popoverId = Math.random().toString(36).slice(-8)
         onClickOutside(popoverRef, event => {
             if (!props.closeOnClickOutside) return
-            const path = (event as PointerEvent & { path: HTMLElement[] }).path
+            const path = (event as PointerEvent & { path: HTMLElement[] })?.path || (event as PointerEvent & { composedPath: () => HTMLElement[] })?.composedPath() || []
             for (const el of path) {
                 const wpPopover = el.getAttribute?.('_wp_popover_')
                 if (wpPopover === popoverId) return
