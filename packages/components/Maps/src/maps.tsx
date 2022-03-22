d64c6aa7040ac2cfc0c2f79b1b3da6b0000434b4
zys8119 <770959294@qq.com>
2022-03-22 17:47:44 +0800

    [add] maps.tsx

--- packages/components/Maps/src/maps.tsx
+++ packages/components/Maps/src/maps.tsx
@@ -1,5 +1,6 @@
-import {defineComponent, ExtractPropTypes} from "vue"
+import {defineComponent, ExtractPropTypes, ref, onMounted} from "vue"
 import {buildProps} from "@wisdom-plus/utils/props";
+import AMapLoader from "@amap/amap-jsapi-loader"
 export const mapsProps = buildProps({
 
 })
@@ -7,9 +8,30 @@ export type MapsProps = ExtractPropTypes<typeof mapsProps>
 export default defineComponent({
     name:"WpMaps",
     props:mapsProps,
+    setup(){
+        const container:any = ref(null)
+        const map:any = ref(null)
+        onMounted(()=>{
+            AMapLoader.load({
+                key:"392d52cc1535e162f0eba5b118cd1c49",
+                version: "2.0",
+                plugins: ['AMap.Scale','s'],
+            }).then(AMap=>{
+                map.value = new AMap.Map(container.value);
+                map.value.addControl(new AMap.Scale());
+            })
+        })
+        return {
+            container,
+        }
+    },
     render(){
-        return (<div>
-            asdas
+        return (<div class={{
+            'wp-maps':true
+        }}>
+            <div class={{
+                'wp-maps-container': true
+            }} ref="container"/>
         </div>)
     }
 })
