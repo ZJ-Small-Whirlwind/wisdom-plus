c3f6d6eb7063c061525e9be4266df9427ee43337
chentao <jooies@qq.com>
2022-03-28 11:15:09 +0800

    [test] add vertical test case

--- packages/components/Spin/test/spin.test.ts
+++ packages/components/Spin/test/spin.test.ts
@@ -12,3 +12,15 @@ describe('Attrs', () => {
         expect(wrapper.find('.wp-spin').classes()).toContain('test')
     })
 })
+
+describe('Props', () => {
+    test('Vertical', () => {
+        const wrapper = mount(Spin, {
+            props: {
+                vertical: true
+            }
+        })
+
+        expect(wrapper.find('.wp-spin').classes()).toContain('wp-spin--vertical')
+    })
+})
