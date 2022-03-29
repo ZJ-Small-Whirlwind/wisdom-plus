e6f3814ca6e22a8572f63e40797bf3e76448ea91
zys8119 <770959294@qq.com>
2022-03-29 15:42:04 +0800

    [add] 图标管理

--- icons/src/App.tsx
+++ icons/src/App.tsx
@@ -1,39 +1,28 @@
-import {defineComponent, ref} from "vue"
+import {defineComponent, ref, h} from "vue"
 import {
     WpImage,
     WpInput,
+    WpSpin
 } from "@wisdom-plus/components"
 import logo from "../../wisdom-plus.png"
 export default defineComponent({
     name:"App",
     setup(){
-        const search = ref()
-        const searchChange = async v=>{
-            const res = await fetch("https://www.iconfont.cn/api/icon/search.json", {
-                "headers": {
-                    "accept": "application/json, text/javascript, */*; q=0.01",
-                    "accept-language": "zh-CN,zh;q=0.9",
-                    "cache-control": "no-cache",
-                    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                    "pragma": "no-cache",
-                    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"",
-                    "sec-ch-ua-mobile": "?0",
-                    "sec-ch-ua-platform": "\"macOS\"",
-                    "sec-fetch-dest": "empty",
-                    "sec-fetch-mode": "cors",
-                    "sec-fetch-site": "same-origin",
-                    "x-csrf-token": "r7Gi4zWH1coRREauhymRcF2K",
-                    "x-requested-with": "XMLHttpRequest"
-                },
-                "referrer": "https://www.iconfont.cn/search/index?searchType=icon&q=%E5%88%A0%E9%99%A4",
-                "referrerPolicy": "strict-origin-when-cross-origin",
-                "body": "q=%E5%88%A0%E9%99%A4&sortType=updated_at&page=1&pageSize=54&fromCollection=-1&fills=&t=1648521786575&ctoken=r7Gi4zWH1coRREauhymRcF2K",
-                "method": "POST",
-                "mode": "cors",
-                "credentials": "include"
-            });
-            console.log(res)
+        const search = ref("删除");
+        const IconList = ref<Array<{
+            show_svg:string
+        }>>([]);
+        const loading = ref(false);
+        const searchChange = async ()=>{
+            loading.value = true;
+            if(search.value){
+                // IconList.value = (await (await fetch(`http://localhost:81/Dome/Iconfont/search?search=${search.value}`)).json()).data;
+            }else {
+
+            }
+            loading.value = false;
         }
+        searchChange()
         return ()=>(<div class={{
             "icons-main":true
         }}>
@@ -45,14 +34,30 @@ export default defineComponent({
                     <WpInput placeholder={'搜索图标'}
                              clearable
                              v-model={search.value}
-                             onInput={searchChange}
-                             onClear={searchChange}
+                             onKeyup={e=>e.code === 'Enter' ? searchChange() : null}
+                             onClear={searchChange()}
                     ></WpInput>
                 </div>
                 <div class={"navs"}>
                     <div>中文</div>
                     <div>帮助文档</div>
                 </div>
+            </div>
+            {loading.value ? <WpSpin></WpSpin> : null}
+            <div class={"icons-container"}>
+                {IconList.value.map((item)=>(<div class={{
+                    "icons-container-item":true,
+                    // "icons-container-item-active":true,
+                }}>
+                    {h("div", {
+                        class:"icons-container-item-icon",
+                        innerHTML:item.show_svg
+                    })}
+                    <div class={{
+                        "icons-container-item-action":true,
+                    }}>保存图标</div>
+                </div>))}
+                {IconList.value.length === 0 ? <div class={'notSearchResult'}>暂无数据！</div> : null}
             </div>
         </div>)
     }
