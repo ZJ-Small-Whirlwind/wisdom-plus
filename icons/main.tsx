112cf903d93e82d0285d0b55c44e5e89d76582a2
zys8119 <770959294@qq.com>
2022-03-30 16:45:32 +0800

    [add] 图标管理

--- icons/main.tsx
+++ icons/main.tsx
@@ -3,6 +3,7 @@ import "./style.scss"
 import "@wisdom-plus/theme-chalk/src/index.scss"
 import route from "./src/router"
 import App from "./src/App"
+import "github-markdown-css/github-markdown-light.css"
 createApp(App)
     .use(route)
     .mount("#app")
