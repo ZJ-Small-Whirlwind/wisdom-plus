929e78c2831d1edd6ecc172795d02fe6837ebb5f
zys8119 <770959294@qq.com>
2022-03-30 10:11:07 +0800

    [add] 图标管理

--- icons/src/App.tsx
+++ icons/src/App.tsx
@@ -1,28 +1,122 @@
-import {defineComponent, ref, h} from "vue"
+import {defineComponent, ref, h, onMounted} from "vue"
 import {
     WpImage,
     WpInput,
-    WpSpin
+    WpSpin,
+    Toast as WpToast,
+    WpEllipsis,
+    Dialog
 } from "@wisdom-plus/components"
 import logo from "../../wisdom-plus.png"
+import * as console from "console";
+type Icon = {
+    id:number
+    show_svg:string
+    name:string
+    font_class:string
+}
 export default defineComponent({
     name:"App",
     setup(){
-        const search = ref("删除");
-        const IconList = ref<Array<{
-            show_svg:string
-        }>>([]);
+        const api = "http://localhost:81/Dome/Iconfont";
+        const getApi = (url:string)=>{
+            return `${api}${url}`
+        }
+        const search = ref("");
+        const MyIconList = ref<Array<{
+            [key:number]:Icon
+        }>>({} as any);
+        const IconList = ref<Array<Icon>>([]);
         const loading = ref(false);
+        const getMyIconList = async ()=>{
+            MyIconList.value = (await (await fetch(getApi(`/getConfigs`))).json()).data;
+        }
         const searchChange = async ()=>{
             loading.value = true;
             if(search.value){
-                IconList.value = (await (await fetch(`http://localhost:81/Dome/Iconfont/search?search=${search.value}`)).json()).data;
+                // 搜索图标
+                IconList.value = (await (await fetch(getApi(`/search?search=${search.value}`))).json()).data;
             }else {
-                // console.log((await (await fetch(`http://localhost:81/Dome/Iconfont/getConfigs`)).json()).data)
+                await getMyIconList();
+                IconList.value = Object.values(MyIconList.value) as any
             }
             loading.value = false;
         }
-        searchChange()
+        const onCopy = function (text) {
+            const callback = function (e) {
+                e.preventDefault();
+                if (e.clipboardData) {
+                    e.clipboardData.setData('text/plain', text);
+                } else {
+                    // @ts-ignore
+                    if (window.clipboardData) {
+                        // @ts-ignore
+                        window.clipboardData.setData('Text', text);
+                    }
+                }
+            }
+            window.addEventListener('copy', callback);
+            document.execCommand('copy');
+            window.removeEventListener('copy', callback);
+        }
+        const IconClick = async (icon:Icon)=>{
+            if(MyIconList.value[icon.id]){
+                // 我的图标
+                onCopy(icon.font_class.toUpperCase())
+                WpToast({
+                    message:`已复制`,
+                    placement:"top",
+                    dark:true
+                })
+            }else {
+                // 添加图标
+                loading.value = true;
+                await fetch(getApi(`/setConfigs`),{
+                    method:"post",
+                    headers:{
+                        "Content-Type":"application/json; charset=utf-8"
+                    },
+                    body:JSON.stringify(icon)
+                })
+                await getMyIconList();
+                loading.value = false;
+                WpToast({
+                    message:"保存成功",
+                    placement:"top",
+                    dark:true
+                })
+            }
+        }
+        const delIcon = (icon:Icon, ev:PointerEvent)=>{
+            ev.stopPropagation();
+            Dialog({
+                title:"删除提示",
+                content:`确定删除该【${icon.name}】图标吗`,
+            }).then(async ()=>{
+                loading.value = true;
+                await fetch(getApi(`/setConfigs`),{
+                    method:"post",
+                    headers:{
+                        "Content-Type":"application/json; charset=utf-8"
+                    },
+                    body:JSON.stringify({
+                        ...icon,
+                        is_delete_wp_icon:true,
+                    })
+                })
+                await getMyIconList();
+                loading.value = false;
+                WpToast({
+                    message:"已删除",
+                    placement:"top",
+                    dark:true
+                })
+            })
+        }
+        onMounted(async ()=>{
+            await getMyIconList();
+            await searchChange();
+        })
         return ()=>(<div class={{
             "icons-main":true
         }}>
@@ -35,7 +129,7 @@ export default defineComponent({
                              clearable
                              v-model={search.value}
                              onKeyup={e=>e.code === 'Enter' ? searchChange() : null}
-                             onClear={searchChange()}
+                             onClear={searchChange}
                     ></WpInput>
                 </div>
                 <div class={"navs"}>
@@ -45,17 +139,37 @@ export default defineComponent({
             </div>
             {loading.value ? <WpSpin></WpSpin> : null}
             <div class={"icons-container"}>
-                {IconList.value.map((item)=>(<div class={{
+                {IconList.value.map((item)=>(<div
+                    onClick={()=>IconClick(item)}
+                    class={{
                     "icons-container-item":true,
-                    // "icons-container-item-active":true,
+                    "icons-container-item-active":MyIconList.value[item.id],
                 }}>
                     {h("div", {
                         class:"icons-container-item-icon",
                         innerHTML:item.show_svg
                     })}
                     <div class={{
-                        "icons-container-item-action":true,
-                    }}>保存图标</div>
+                        "icons-container-item-icon-name": true,
+                    }}><WpEllipsis>{item.name}</WpEllipsis></div>
+                    <div class={{
+                        "icons-container-item-icon-name": true,
+                    }}><WpEllipsis>({item.font_class})</WpEllipsis></div>
+                    {MyIconList.value[item.id] ?
+                        [
+                            <div class={{
+                                "icons-container-item-action": true,
+                            }}>复制图标名称</div>,
+                            <div class={{
+                                "icons-container-item-operate": true,
+                            }}>
+                                <span onClick={(ev)=>delIcon(item,ev)}>删除图标</span>
+                            </div>
+                        ]
+                        :
+                        <div class={{
+                            "icons-container-item-action": true,
+                        }}>保存图标</div> }
                 </div>))}
                 {IconList.value.length === 0 ? <div class={'notSearchResult'}>暂无数据！</div> : null}
             </div>
--- packages/icons/index.tsx
+++ packages/icons/index.tsx
@@ -1,1 +1,3 @@
 export { default as CHANGYONGGOUPIAORENSHANCHU } from "./src/CHANGYONGGOUPIAORENSHANCHU"
+export { default as BAOCUNDAOZHUOMIAN } from "./src/BAOCUNDAOZHUOMIAN"
+export { default as BAOCUN } from "./src/BAOCUN"
